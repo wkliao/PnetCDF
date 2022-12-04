df07e830d83437212714c677027ef2d79135252d
Kaiyuan Hou <khou2020@outlook.com>
2022-12-04 16:56:08 -0600

    enable chunk driver when chunking hint is set

--- src/dispatchers/file.c
+++ src/dispatchers/file.c
@@ -898,6 +898,9 @@ ncmpi_create(MPI_Comm    comm,
 #if PNETCDF_BURST_BUFFERING == 1
     int enable_bb_driver=0;
 #endif
+#ifdef ENABLE_CHUNKING
+    int enable_chk_driver=0;
+#endif
 
 #if PNETCDF_PROFILING == 1
     {
@@ -1061,6 +1064,18 @@ ncmpi_create(MPI_Comm    comm,
             enable_bb_driver = 1;
     }
 #endif
+#ifdef ENABLE_CHUNKING
+    if (combined_info != MPI_INFO_NULL) {
+        char value[MPI_MAX_INFO_VAL];
+        int flag;
+
+        /* check if nc_chunking is enabled */
+        MPI_Info_get(combined_info, "nc_chunking", MPI_MAX_INFO_VAL-1,
+                     value, &flag);
+        if (flag && strcasecmp(value, "enable") == 0)
+            enable_chk_driver = 1;
+    }
+#endif
 
     /* Use environment variable and cmode to tell the file format
      * which is later used to select the right driver.
@@ -1137,6 +1152,11 @@ ncmpi_create(MPI_Comm    comm,
 #if PNETCDF_BURST_BUFFERING == 1
     if (enable_bb_driver)
         driver = ncbbio_inq_driver();
+    else
+#endif
+#ifdef ENABLE_CHUNKING
+    if (enable_chk_driver)
+        driver = ncchkio_inq_driver();
     else
 #endif
         /* default is the driver built on top of MPI-IO */
@@ -1219,6 +1239,9 @@ ncmpi_open(MPI_Comm    comm,
 #endif
 #if PNETCDF_BURST_BUFFERING == 1
     int enable_bb_driver=0;
+#endif
+#ifdef ENABLE_CHUNKING
+    int enable_chk_driver=0;
 #endif
 
 #if PNETCDF_PROFILING == 1
@@ -1419,6 +1442,18 @@ ncmpi_open(MPI_Comm    comm,
             enable_bb_driver = 1;
     }
 #endif
+#ifdef ENABLE_CHUNKING
+    if (combined_info != MPI_INFO_NULL) {
+        char value[MPI_MAX_INFO_VAL];
+        int flag;
+
+        /* check if nc_chunking is enabled */
+        MPI_Info_get(combined_info, "nc_chunking", MPI_MAX_INFO_VAL-1,
+                     value, &flag);
+        if (flag && strcasecmp(value, "enable") == 0)
+            enable_chk_driver = 1;
+    }
+#endif
 
 #if PNETCDF_DRIVER_NETCDF4 == 1
     if (format == NC_FORMAT_NETCDF4_CLASSIC || format == NC_FORMAT_NETCDF4) {
@@ -1450,6 +1485,11 @@ ncmpi_open(MPI_Comm    comm,
 #if PNETCDF_BURST_BUFFERING == 1
     if (enable_bb_driver)
         driver = ncbbio_inq_driver();
+    else
+#endif
+#ifdef ENABLE_CHUNKING
+    if (enable_chk_driver)
+        driver = ncchkio_inq_driver();
     else
 #endif
     {
