606b9150aa60cb9b767c5b319f39d7c4140519a3
wkliao <wkliao@users.noreply.github.com>
2024-11-29 11:54:45 -0600

    ncmpio_file_misc.c Skip creating an MPI_COMM_SELF file handler when using internal ROMIO
    This is because internal ROMIO uses its own _File_set_view and always
    passes MPI_INFO_NULL for argument info, which makes _File_set_view an
    independent call. Thus there is no need to create a separate file
    handler. In other words, collective-independent mode can be eliminated
    completely and only one file handler is sufficient.

--- src/drivers/ncmpio/ncmpio_file_misc.c
+++ src/drivers/ncmpio/ncmpio_file_misc.c
@@ -151,6 +151,8 @@ ncmpio_begin_indep_data(void *ncdp)
     /* raise independent flag */
     fSet(ncp->flags, NC_MODE_INDEP);
 
+    if (ncp->fstype != ADIO_UFS) return NC_NOERR;
+
     /* PnetCDF's default mode is collective. MPI file handle, collective_fh,
      * will never be MPI_FILE_NULL. We must use a separate MPI file handle
      * opened with MPI_COMM_SELF, because MPI_File_set_view is a collective
