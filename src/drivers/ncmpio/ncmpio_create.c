5686a76450e429ccec48a7d3d86afe0cd252f106
Wei-keng Liao <wkliao@users.noreply.github.com>
2024-12-28 18:12:21 -0800

    For Lustre, cb_buffer_size must be at least striping_unit

--- src/drivers/ncmpio/ncmpio_create.c
+++ src/drivers/ncmpio/ncmpio_create.c
@@ -353,6 +353,13 @@ printf("xxxx %s %d: use_mpi_io = %d\n",__func__,__LINE__,use_mpi_io);
      */
     ncmpio_set_pnetcdf_hints(ncp, user_info, info_used);
 
+    if (adio_fh->hints->cb_buffer_size < adio_fh->hints->striping_unit) {
+        /* collective buffer size must be at least file striping size */
+        adio_fh->hints->cb_buffer_size = adio_fh->hints->striping_unit;
+        sprintf(value, "%d", adio_fh->hints->cb_buffer_size);
+        MPI_Info_set(info_used, "cb_buffer_size", value);
+    }
+
 #ifdef PNETCDF_DEBUG
     /* PNETCDF_DEBUG is set at configure time, which will be overwritten by
      * the run-time environment variable PNETCDF_SAFE_MODE */
